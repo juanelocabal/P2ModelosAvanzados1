081cc0be74a6060b838286704a7f1ea3160f083e
David Tavera Sánchez <d.tavera2269@uniandes.edu.co>
2020-06-29 11:13:19 -0500

    Create model_deployment

---
+++ model_deployment/api.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+from flask import Flask
+from flask_restplus import Api, Resource, fields
+from sklearn.externals import joblib
+from p2model_deployment import predict_price
+
+app = Flask(__name__)
+
+api = Api(
+    app, 
+    version='1.0', 
+    title='Price Prediction API G7',
+    description='Angie Paola Chacon Pinzon: 201012536
+Jahir Stevens Rodriguez Riveros: 201819361
+Juan Andres Cabal Jaramillo: 200620778
+David Tavera Sánchez: 201016123')
+
+ns = api.namespace('predict', 
+     description='Price Regressor')
+   
+parser = api.parser()
+
+parser.add_argument(
+    'Year', 
+    type=int, 
+    required=True, 
+    help='Year of the car to be analyzed', 
+    location='args')
+
+parser.add_argument(
+    'Mileage', 
+    type=int, 
+    required=True, 
+    help='Mileage of the car to be analyzed', 
+    location='args')
+
+parser.add_argument(
+    'State', 
+    type=str, 
+    required=True, 
+    help='State of the car to be analyzed', 
+    location='args')
+
+parser.add_argument(
+    'Make', 
+    type=str, 
+    required=True, 
+    help='Maker of the car to be analyzed', 
+    location='args')
+
+parser.add_argument(
+    'Model', 
+    type=str, 
+    required=True, 
+    help='Model of the car to be analyzed', 
+    location='args')
+
+
+resource_fields = api.model('Resource', {
+    'price': fields.String,
+})
+
+@ns.route('/')
+class PriceApi(Resource):
+
+    @api.doc(parser=parser)
+    @api.marshal_with(resource_fields)
+    def get(self):
+        args = parser.parse_args()
+        
+        return {
+         "price": predict_price(args['Year'],args['Mileage'],args['State'],args['Make'],args['Model'])
+        }, 200
+    
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
